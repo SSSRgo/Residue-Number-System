f2ed215a36a1db634d75e81dda64bdc3d38c6f7f
SSSRgo <1731383841@qq.com>
2024-10-26 16:24:13 -0400

    Initial commit

---
+++ similarity_kernel_example.py
@@ -0,0 +1,28 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Parameters
+N = 7  # Number of roots of unity
+x = 1  # Fixed x value for calculation
+delta_x_range = np.arange(-10, 11)  # Range of delta_x values
+
+# Define theta values for the N roots of unity
+theta = 2 * np.pi * np.arange(N) / N
+
+# Compute z(x) and z(x + delta_x) for each delta_x in the specified range
+similarities = []
+for delta_x in delta_x_range:
+    z_x = np.exp(1j * theta * x)
+    z_x_delta = np.exp(1j * theta * (x + delta_x))
+    similarity = np.real(np.dot(z_x, np.conj(z_x_delta))) / N
+    similarities.append(similarity)
+
+# Plotting the similarity kernel
+plt.figure(figsize=(10, 5))
+plt.plot(delta_x_range, similarities, marker='o')
+plt.title("Similarity Kernel for N=7")
+plt.xlabel(r"$\Delta x$")
+plt.ylabel(r"$z(x) \cdot z(x + \Delta x)$")
+plt.grid(True)
+plt.show()
